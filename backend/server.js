03f8e19e9f797c628def2124ba3e9bb0c65a650e
NISHAT-ANNAN-MIM <nishat.annan.mim.2003@gmail.com>
2025-04-15 21:16:13 +0600

    first commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app =express();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+app.get("/",(req,res) => {
+    //root route http://localhost:5000/
+    res.send("hello worldkkkkk");
+});
+app.listen(PORT, () => console.log(`server Running on ${PORT}`));
