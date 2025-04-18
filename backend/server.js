70e4b0a37d5a1df80650da0c06f0b0d6d24de8f1
NISHAT-ANNAN-MIM <nishat.annan.mim.2003@gmail.com>
2025-04-18 09:16:59 +0600

    1st part(module1,1) backend completed

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,36 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app =express();
-dotenv.config();
 const PORT = process.env.PORT || 5000;
-app.get("/",(req,res) => {
-    //root route http://localhost:5000/
-    res.send("hello worldkkkkk");
+
+dotenv.config();
+
+app.use(express.json());//to parse the incoming request with json payloads from request.body
+app.use(cookieParser());//to parse the incoming request with cookies
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+
+
+
+// app.get("/",(req,res) => {
+//     //root route http://localhost:5000/
+//     res.send("hello world");
+// });
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`server Running on ${PORT}`);
 });
-app.listen(PORT, () => console.log(`server Running on ${PORT}`));
+
